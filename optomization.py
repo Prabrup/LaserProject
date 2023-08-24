35b6e2e4d493eb64ddc04a81fb8cf26c8bca2eab
Prabrup <53101299+Prabrup@users.noreply.github.com>
2023-08-24 15:34:46 +0100

    Update optomization.py
    now constrained to a cartesian axis

--- optomization.py
+++ optomization.py
@@ -2,28 +2,63 @@ import numpy as np
 from scipy.optimize import minimize,least_squares
 from sklearn.metrics import mean_squared_error
 
+#d = [xc yc xd yd zd]
+#d = [0   1  2  3  4]
+def distances(d):
+    #Rosenbrock function
+    AC = np.sqrt(d[0]**2+d[1]**2)
+    AD = np.sqrt(d[2]**2+d[3]**2+d[4]**2)
+    BC = np.sqrt((np.sqrt(14604)-d[0])**2 + d[1]**2)
+    CD = np.sqrt((d[0]-d[2])**2+(d[1]-d[3])**2 +d[4]**2)
+    BD = np.sqrt((np.sqrt(14604)-d[2])**2+d[3]**2+d[4]**2)
+    return AC,AD,BC,CD,BD
 
-#d = [xa ya za xb yb zb xc yc zc xd yd zd]
-#d = [0   1  2  3  4  5  6 7  8  9  10 11]
 def rosen(d):
     #Rosenbrock function
-    AB = np.sqrt((d[0]-d[3])**2 + (d[1]-d[4])**2 + (d[2]-d[5])**2)
-    AC = np.sqrt((d[0]-d[6])**2 + (d[1]-d[7])**2 + (d[2]-d[8])**2)
-    AD = np.sqrt((d[0]-d[9])**2 + (d[1]-d[10])**2 + (d[2]-d[11])**2)
-    BC = np.sqrt((d[3]-d[6])**2 + (d[4]-d[7])**2 + (d[5]-d[8])**2)
-    DC = np.sqrt((d[6]-d[9])**2 + (d[7]-d[10])**2 + (d[8]-d[11])**2)
-    BD = np.sqrt((d[3]-d[9])**2 + (d[4]-d[10])**2 + (d[5]-d[11])**2)
-    return (AB-np.sqrt(14604))**2 + (AC-np.sqrt(9891))**2 + (AD-np.sqrt(11889))**2 + (BC-np.sqrt(6664))**2 + (DC-np.sqrt(2094))**2
+    AC,AD,BC,CD,BD = distances(d)
+    return  ((AC-np.sqrt(9891))**2 + (AD-np.sqrt(11889))**2 + (BC-np.sqrt(6664))**2 + (CD-np.sqrt(2094))**2 + (BD-np.sqrt(9578))**2)
 
-d0 = [100,2,100,11,26,7,50,70,38,90,90,23]
-d_correct = [82,3,96,9,28,3,57,82,41,94,75,15]
+
+#d0 = [[50,70,90,90,23]]
+limit = 100
+d0 =[[np.random.randint(limit, size=5)],[np.random.randint(limit, size=5)],[np.random.randint(limit, size=5)],[np.random.randint(limit, size=5)],[np.random.randint(limit, size=5)],[np.random.randint(limit, size=5)],[np.random.randint(limit, size=5)]]
+
+
 Pos = []
-list = ["nelder-mead","powell","CG","BFGS","L-BFGS-B","TNC","COBYLA","SLSQP","trust-constr"]
+#list = ["nelder-mead","powell","CG","BFGS","L-BFGS-B","TNC","COBYLA","SLSQP","trust-constr"]
+#list = ["nelder-mead"]
 #o = least_squares(rosen,d0,method="lm")
-for j,i in enumerate(list):
-    a = minimize(rosen, d0,method=i,options={"disp":True})
+for j,i in enumerate(d0):
+    a = minimize(rosen, i,method="nelder-mead",options={"disp":True})
     Pos.append(a.x)
 
+Pos_n=np.array(Pos).reshape((len(d0),5))
+#print(np.array(Pos).reshape((len(d0)*6,5)))
 
-Pos_n = np.array(Pos).reshape((len(list)*4,3))
+#print([rosen(Pos_n[0]),rosen(Pos_n[1]),rosen(Pos_n[2]),rosen(Pos_n[3]),rosen(Pos_n[4]),rosen(Pos_n[5])])
+print(d0)
+print(Pos_n)
+#print(rosen(Pos_n[1]))
+"""
+import matplotlib.pyplot as plt
 
+print(Pos)
+
+x = Pos_n[:, 0]
+y = Pos_n[:, 1]
+z = Pos_n[:, 2]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Scatter plot of points
+ax.scatter(x, y, z, c='b', marker='o')
+
+# Set labels for the axes
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+# Show the plot
+plt.show()
+"""
