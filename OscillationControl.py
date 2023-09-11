683a82e37d93e8dbd4f18ee5c6fec6c4c60fdd86
Prabrup <53101299+Prabrup@users.noreply.github.com>
2023-09-11 11:55:46 +0100

    Update OscillationControl.py

--- OscillationControl.py
+++ OscillationControl.py
@@ -5,43 +5,40 @@ from optoMDC.registers.mre2_registers import ResonantAmplitudeControl
 import optoMDC
 mre2 = optoMDC.connect()
 
-#To configure channel in open loop mode...
+#Initialise first mirror and boths its axis in open loop
 
 ch_0 = mre2.Mirror.Channel_0
 
-ch_0.SignalGenerator.SetAsInput()                    # (1) here we tell the Manager that the sig gen is the desired input
-ch_0.InputConditioning.SetGain(1.0)                  # (2) here we tell the Manager some input conditioning parameters
-ch_0.SetControlMode(optoMDC.Units.CURRENT)           # (3) here we tell the Manager that our input will be in units of current. This is open-loop mode
-ch_0.LinearOutput.SetCurrentLimit(0.7)               # (4) here we tell the Manager to limit the current to 700mA (default)
+ch_0.SignalGenerator.SetAsInput()                    
+ch_0.InputConditioning.SetGain(1.0)
+ch_0.SetControlMode(optoMDC.Units.CURRENT)    
+ch_0.LinearOutput.SetCurrentLimit(0.7)               
 
-ch_0.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+ch_0.Manager.CheckSignalFlow()                      
 
 ch_1 = mre2.Mirror.Channel_1
 
-ch_1.SignalGenerator.SetAsInput()                    # (1) here we tell the Manager that the sig gen is the desired input
-ch_1.InputConditioning.SetGain(1.0)                  # (2) here we tell the Manager some input conditioning parameters
-ch_1.SetControlMode(optoMDC.Units.CURRENT)           # (3) here we tell the Manager that our input will be in units of current. This is open-loop mode
-ch_1.LinearOutput.SetCurrentLimit(0.7)               # (4) here we tell the Manager to limit the current to 700mA (default)
+ch_1.SignalGenerator.SetAsInput()              
+ch_1.InputConditioning.SetGain(1.0)       
+ch_1.SetControlMode(optoMDC.Units.CURRENT)     
+ch_1.LinearOutput.SetCurrentLimit(0.7)           
 
-ch_1.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+ch_1.Manager.CheckSignalFlow()               
 
 
-#The channel is configured. The output of the Signal Generator will now proceed through the signal flow mentioned above.
-
-#Therefore, next we will configure the output of the Signal Generator.
-
+#Initialise second mirror and boths its axis in open loop
 sg_0 = mre2.Mirror.Channel_0.SignalGenerator
 
-sg_0.SetUnit(optoMDC.Units.CURRENT)                 # here we set the sig gen to output in units of current (This must match the control mode!)
-sg_0.SetShape(optoMDC.Waveforms.TRIANGLE)         # here we set the sig gen output waveform type
-sg_0.SetFrequency(2)                             # here we set the frequency in Hz
-sg_0.SetAmplitude(0.010)                            # here we set the amplitude in Amps
+sg_0.SetUnit(optoMDC.Units.CURRENT)                
+sg_0.SetShape(optoMDC.Waveforms.TRIANGLE)        
+sg_0.SetFrequency(2)                           
+sg_0.SetAmplitude(0.010)                            
 sg_0.Run()
 
 sg_1 = mre2.Mirror.Channel_1.SignalGenerator
 
-sg_1.SetUnit(optoMDC.Units.CURRENT)                 # here we set the sig gen to output in units of current (This must match the control mode!)
-sg_1.SetShape(optoMDC.Waveforms.PULSE)         # here we set the sig gen output waveform type
-sg_1.SetFrequency(2.0)                             # here we set the frequency in Hz
-sg_1.SetAmplitude(0.010)                            # here we set the amplitude in Amps
+sg_1.SetUnit(optoMDC.Units.CURRENT)                
+sg_1.SetShape(optoMDC.Waveforms.PULSE)        
+sg_1.SetFrequency(2.0)                            
+sg_1.SetAmplitude(0.010)                            
 sg_1.Run()
